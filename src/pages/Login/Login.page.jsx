f3526e0cb0362244790bf289bf8c06915419171b
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-07-17 22:25:05 -0300

    Criando componente de formulário de login

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,9 +1,10 @@
+import { FormLoginComponent } from "../../components/Form/Login/Login.component";
 
 
 export function LoginPage() {
     return (
     <>
-      <p>App is render</p>
+      <FormLoginComponent/>
     </>
   )
 }
