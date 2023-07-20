efe67464ab7bd46a9b77b0cadac59b5f04b5aaf0
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-07-20 13:19:53 -0300

    Criando arquivo global de estilos

---
+++ src/global.style.jsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding:0;
+    box-sizing: border-box;
+    outline: none;
+    font-family: 'Segoe UI', Arial, Helvetica, sans-serif;
+}
+`
