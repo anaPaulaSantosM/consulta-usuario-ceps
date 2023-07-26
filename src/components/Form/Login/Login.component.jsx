d42c8846b986bf5b5081d3046d98f4d8b0c57bf5
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-07-25 21:43:31 -0300

    Criando formulario de login

--- src/components/Form/Login/Login.component.jsx
+++ src/components/Form/Login/Login.component.jsx
@@ -1,25 +1,57 @@
 import { useNavigate } from "react-router-dom";
+import * as Styled from "./Login.style";
+import { useState } from "react";
 
 export const FormLoginComponent = () => {
 
+    const [data, setData] = useState({
+        email: '',
+        password: '',
+    });
+
     const navigate = useNavigate();
+
+    const isDisabled = (event) => {
+        return !data.email || !data.password || !data.email.includes('@') || data.password.length < 8;
+    }
+
+    const handleInput = (event) => {
+        event.preventDefault();
+        const { value, id } = event.target;
+
+        setData ({ ...data, [id]: value});
+    }
 
     const redirectToLogin = () => {
         navigate('/home')
     }
 
     return (
-        <form onSubmit={redirectToLogin}>
+        <Styled.Form onSubmit={redirectToLogin}>
+            
+            <Styled.Header>
+                <Styled.Title>Login</Styled.Title>
+                <Styled.SubTitle>Texto</Styled.SubTitle>
+            </Styled.Header>
+
+            <Styled.InputGroup>
             <div className="input-group">
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" placeholder="Digite seu email"/>
+                <input type="email" id="email" onInput={handleInput} placeholder="Digite seu email"/>
             </div>
             <div className="input-group">
                 <label htmlFor="password">Senha</label>
-                <input type="password" id="password" placeholder="Digite sua senha"/>
+                <input type="password" id="password" onInput={handleInput} placeholder="Digite sua senha"/>
             </div>
+            </Styled.InputGroup>
 
-            <button type="submit">Logar</button>
-        </form>
+            <Styled.Button type="submit" disabled={isDisabled()}>Entrar</Styled.Button>
+
+            <Styled.Action>
+
+                <Styled.EsqueciSenha>Esqueci minha senha</Styled.EsqueciSenha>
+                <Styled.Button $outlined= {true} type="button">Criar conta</Styled.Button>
+            </Styled.Action>
+        </Styled.Form>
     )
 }
